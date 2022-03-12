eed0c945b9780e7202fc0a2e78d50f6912e70e58
Yash <yashsi3007@gmail.com>
2022-03-12 16:17:08 +0530

    delete a theme and modify packer-config file

--- nvim/.config/nvim/lua/packer-config/init.lua
+++ nvim/.config/nvim/lua/packer-config/init.lua
@@ -1,6 +1,5 @@
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use 'EdenEast/nightfox.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
