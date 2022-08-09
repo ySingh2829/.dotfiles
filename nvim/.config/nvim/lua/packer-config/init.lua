d1392998fa10734a1942bdfd53e438bbabff13f7
Yash <yashsi3007@gmail.com>
2022-08-09 23:16:04 +0530

    major change

--- nvim/.config/nvim/lua/packer-config/init.lua
+++ nvim/.config/nvim/lua/packer-config/init.lua
@@ -15,10 +15,10 @@ return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
   use {
     "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim"
   }
-  use "williamboman/mason-lspconfig.nvim"
   use 'onsails/lspkind-nvim'
-  use 'williamboman/nvim-lsp-installer'
+  --use 'williamboman/nvim-lsp-installer'
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
